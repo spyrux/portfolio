002730c329850f83dd30891d1db93bf4e1c9f62d
William Cui <37138564+spyrux@users.noreply.github.com>
2023-08-28 00:45:10 -0700

    fixed navbar bg

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,13 +7,13 @@ const Header = () => {
   const handleToggle = () => setToggle(!toggle);
 
   return (
-    <header className="flex justify-between px-5 py-5 bg-primary text-white w-full fixed z-10">
-      <a href="/" className=" opacity-0 md:opacity-100 logo text-2xl font-bold text-accent z-50">
+    <header className="flex justify-between px-5 bg-primary text-white w-full fixed z-10">
+      <a href="/" className=" opacity-0 md:opacity-100 md:py-5 logo text-2xl font-bold text-accent z-50">
         WC
       </a>
 
       {/* Desktop Nav */}
-      <nav className="hidden md:block">
+      <nav className="hidden md:block md:py-5">
         <ul className="flex space-x-10 font-mono text-xl px-6">
           <li>
             <a href ="/#about" >About</a>
@@ -34,9 +34,9 @@ const Header = () => {
       </nav>
 
       {/* Mobile Nav */}
-      <nav className={toggle ? ' block mobile-nav left-0 bg-primary' : 'mobile-nav left-[100%]'}>
+      <nav className={toggle ? ' block mobile-nav left-0 bg-primary h-screen' : 'mobile-nav left-[100%]'}>
         
-        <ul className="flex flex-col font-mono text-3xl py-10 m-12 space-y-2">
+        <ul className="flex flex-col font-mono text-3xl py-20 m-12 space-y-2">
             
             <a href ="/#about" >About</a>
           
@@ -56,7 +56,7 @@ const Header = () => {
       </nav>
 
       {/* Toggle button */}
-      <button onClick={handleToggle} className="block md:hidden z-50">
+      <button onClick={handleToggle} className="block md:hidden py-10 z-50">
         {!toggle ? <AiOutlineMenu size={30} /> : <AiOutlineClose size={30} />}
       </button>
     </header>
